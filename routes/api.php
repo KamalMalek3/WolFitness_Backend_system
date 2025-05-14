96cc9a0fcc6d64c5b55abb21ce92e3adcad3c784
kamal <kamalmalek3@outlook.com>
2025-05-14 16:36:27 +0300

    fixed api

--- routes/api.php
+++ routes/api.php
@@ -15,44 +15,38 @@ use App\Http\Controllers\WorkoutController;
 |--------------------------------------------------------------------------
 */
 
-// Public routes - no authentication needed
-Route::prefix('auth')->group(function () {
-    // Register with personal details
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login',    [AuthController::class, 'login']);
-});
+// Public
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login',    [AuthController::class, 'login']);
 
-// Protected routes - require authentication
+// Protected
 Route::middleware('auth:sanctum')->group(function () {
-    // Auth routes
+    // Auth
     Route::post('/auth/logout', [AuthController::class, 'logout']);
-    
-    // Current user
     Route::get('/user/profile', [AuthController::class, 'profile']);
     Route::put('/user/profile', [UserController::class, 'updateProfile']);
-    
-    // User personal details
-    Route::get('/user/personal-details',    [UserPersonalDetailController::class, 'getUserDetails']);
-    Route::post('/user/personal-details',   [UserPersonalDetailController::class, 'storeUserDetails']);
-    Route::put('/user/personal-details',    [UserPersonalDetailController::class, 'updateUserDetails']);
-    Route::get('/user/bmi',                 [UserPersonalDetailController::class, 'getCurrentUserBMI']);
 
-    Route::get('/progress', [UserProgressController::class, 'index']);
-    Route::post('/progress', [UserProgressController::class, 'store']);
-Route::get('/progress/{id}', [UserProgressController::class, 'show']);
-Route::put('/progress/{id}', [UserProgressController::class, 'update']);
+    // Personal Details
+    Route::get('/user/personal-details',  [UserPersonalDetailController::class, 'getUserDetails']);
+    Route::post('/user/personal-details', [UserPersonalDetailController::class, 'storeUserDetails']);
+    Route::put('/user/personal-details',  [UserPersonalDetailController::class, 'updateUserDetails']);
+    Route::get('/user/bmi',               [UserPersonalDetailController::class, 'getCurrentUserBMI']);
 
+    // User Progress
+    Route::get('/progress',        [UserProgressController::class, 'index']);
+    Route::post('/progress',       [UserProgressController::class, 'store']);
+    Route::get('/progress/{id}',   [UserProgressController::class, 'show']);
+    Route::put('/progress/{id}',   [UserProgressController::class, 'update']);
 
-Route::get('/schedule', [WeeklyScheduleController::class, 'index']);
-Route::post('/schedule', [WeeklyScheduleController::class, 'store']);
-Route::get('/schedule/{id}', [WeeklyScheduleController::class, 'show']);
-Route::put('/schedule/{id}', [WeeklyScheduleController::class, 'update']);
+    // Weekly Schedule
+    Route::get('/schedule',      [WeeklyScheduleController::class, 'index']);
+    Route::post('/schedule',     [WeeklyScheduleController::class, 'store']);
+    Route::get('/schedule/{id}', [WeeklyScheduleController::class, 'show']);
+    Route::put('/schedule/{id}', [WeeklyScheduleController::class, 'update']);
 
-Route::get('/workouts', [WorkoutController::class, 'index']);
-Route::post('/workouts', [WorkoutController::class, 'store']);
-Route::get('/workouts/{id}', [WorkoutController::class, 'show']);
-Route::delete('/workouts/{id}', [WorkoutController::class, 'destroy']);
+    // Workouts
+    Route::get('/workouts',        [WorkoutController::class, 'index']);
+    Route::post('/workouts',       [WorkoutController::class, 'store']);
+    Route::get('/workouts/{id}',   [WorkoutController::class, 'show']);
+    Route::delete('/workouts/{id}',[WorkoutController::class, 'destroy']);
 });
-
-
-
